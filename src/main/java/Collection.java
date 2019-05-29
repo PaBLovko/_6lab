8e7f82bab09d03b8280345a21ad9feb26d33ab2a
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 08:16:10 +0300

    Update Collection.java

--- src/main/java/Collection.java
+++ src/main/java/Collection.java
@@ -7,21 +7,15 @@ import java.util.stream.Collectors;
 public class Collection {
 
     public static double average(List<Integer> listInt) throws MyException{
-        try {
-            return listInt.stream().mapToInt(e -> e).average().orElseThrow(NoSuchElementException::new);
-        }catch (NoSuchElementException e) {throw new MyException(ErrorCode.NotFindElem);}
+        if(listInt.isEmpty()){
+            throw new MyException(ErrorCode.NotFindElem);
+        }
+        return listInt.stream().mapToInt(e -> e).average().orElseThrow(NoSuchElementException::new);
     }
 
-    public static int listLength(List<Integer> listInt) {
-        return listInt.size();
-    }
+    public static double listLength(List<Integer> listInt) { return listInt.stream().count(); }
 
-    public static List<String> strToChar(List<String> listStr) throws MyException {
-        try {
-            if(listStr.isEmpty()){
-                throw new NoSuchElementException();
-            }
-            return listStr.stream().map(e->e.split("")).flatMap(e->Arrays.stream(e)).collect(Collectors.toList());
-        }catch (NoSuchElementException e) {throw new MyException(ErrorCode.NotFindElem);}
+    public static List<String> strToChar(List<String> listStr) {
+        return listStr.stream().map(e->e.split("")).flatMap(e->Arrays.stream(e)).collect(Collectors.toList());
     }
 }
