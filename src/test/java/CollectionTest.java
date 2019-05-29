c624247bf4b7ecec6afdd4913df4c8272a25a525
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 08:16:46 +0300

    Update CollectionTest.java

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -8,27 +8,20 @@ import java.util.logging.Logger;
 import static org.junit.Assert.assertEquals;
 
 public class CollectionTest {
-    private static Logger log = Logger.getLogger(CollectionTest.class.getName());
 
-    @Test
-    public void average(){
-        try {
-            assertEquals("Среднее значение коллекции",3.0, Collection.average(Arrays.asList(1,2,3,4,5)),0.01);
-        }catch (MyException e) {log.log(Level.SEVERE, "Error!", e);}
+    @Test(expected = MyException.class)
+    public void average() throws MyException {
+        Collection.average(Arrays.asList());
     }
 
     @Test
-    public void listLength(){
-       // try {
-            assertEquals("Длина коллекции",5, Collection.listLength(Arrays.asList(1,2,3,4,5)));
-        //}catch (MyException e) {log.log(Level.SEVERE, "Error!", e);}
+    public void listLength() {
+        assertEquals("Длина коллекции", 5, Collection.listLength(Arrays.asList(1, 2, 3, 4, 5)), 0.01);
     }
 
     @Test
-    public void strToChar(){
-        try {
-            assertEquals("Перевод из коллекции строки в коллекцию символов", Arrays.asList("1", "+", "1", "2", ",", "4", "3", "4", "5"),
-                    Collection.strToChar(Arrays.asList("1+1", "2,4", "3", "4", "5")));
-        }catch (MyException e) {log.log(Level.SEVERE, "Error!", e);}
+    public void strToChar() {
+        assertEquals("Перевод из коллекции строки в коллекцию символов", Arrays.asList("1", "+", "1", "2", ",", "4", "3", "4", "5"),
+                Collection.strToChar(Arrays.asList("1+1", "2,4", "3", "4", "5")));
     }
 }
